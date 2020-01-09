7c9cab0f65bee3aca1656b7e440f375d4bccb1b2
Redytel-iTUC <59689637+Redytel-iTUC@users.noreply.github.com>
2020-01-09 13:16:47 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿using Apache.Ignite.Core;
+using Apache.Ignite.Core.Cache;
+using System;
+using System.Linq;
+
+namespace Ignite_LinQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create the List of Employees
+            Employee[] listEmployee = new Employee[4];
+            listEmployee[0] = new Employee(1, "Employee1", "11111111A");
+            listEmployee[0] = new Employee(1, "Employee2", "22222222B");
+            listEmployee[0] = new Employee(1, "Employee3", "33333333C");
+            listEmployee[0] = new Employee(1, "Employee4", "44444444D");
+
+            // Start Ignite Node
+            IIgnite ignite = Ignition.Start();
+
+            // Create the Sharing Memory Cache between Nodes for Employees
+            ICache<int, Employee> employeeCache = ignite.GetOrCreateCache<int, Employee>("myCache");
+
+            // Publish the list in cache
+            for (int i = 0; i < 4; i++)
+            {
+                employeeCache.Put(listEmployee[i].Id, listEmployee[i]);
+            }
+
+            // Ask for a name to search
+            Console.WriteLine("Type the name of employeer");
+            String name = Console.ReadLine();
+
+
+            // Looking for it on cache. Can be used 2 methods
+            //  1- Lambda
+            var queryLambda = employeeCache.Where(emp => (emp.Value.Name == name)).Select(emp => emp.Value.DNI);
+
+            // 2 -  LINQ
+            var queryLinQ = (from x in employeeCache where x.Value.Name == name select x.Value.DNI);
+
+
+            // Showing the DNI associated to employee
+            Console.WriteLine("The employee " + name + " has the DNI: " + queryLinQ.FirstOrDefault());
+
+            // Wait for the window doesn't close
+            Console.ReadKey();
+        }
+    }
+
+    public class Employee
+    {
+        public int Id;
+        public String Name;
+        public String DNI;
+
+        public Employee(int id, String n, String d)
+        {
+            this.Id = id;
+            this.Name = n;
+            this.DNI = d;
+        }
+    }
+}
